2a8e03b4b4d640f2a1a3d451500c96ce9d74683f
gradoslovar <gradoslovar@outlook.com>
2022-03-01 11:26:29 +0100

    test

--- agent/ubuntu/setup.sh
+++ agent/ubuntu/setup.sh
@@ -9,17 +9,17 @@ apt-get update
 # Install security updates:
 apt-get -y upgrade
 
-# ####
-# apt-get install -y --no-install-recommends \
-#     ca-certificates \
-#     curl \
-#     jq \
-#     git \
-#     iputils-ping \
-#     libcurl4 \
-#     libunwind8 \
-#     netcat \
-#     libssl1.0
+####
+apt-get install -y --no-install-recommends \
+    ca-certificates \
+    curl \
+    jq \
+    git \
+    iputils-ping \
+    libcurl4 \
+    libunwind8 \
+    netcat \
+    libssl1.0
 
 # rm -rf /var/lib/apt/lists/* \
 # curl -LO https://dl.k8s.io/release/v1.23.0/bin/linux/amd64/kubectl \
