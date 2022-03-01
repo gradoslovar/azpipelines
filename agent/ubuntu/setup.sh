2c1e30c16e4bbab06a4006a1cd98d69d48d958fd
gradoslovar <gradoslovar@outlook.com>
2022-03-01 03:42:54 +0100

    test

--- agent/ubuntu/setup.sh
+++ agent/ubuntu/setup.sh
@@ -9,26 +9,26 @@ apt-get update
 # Install security updates:
 apt-get -y upgrade
 
-####
-apt-get install -y --no-install-recommends \
-    ca-certificates \
-    curl \
-    jq \
-    git \
-    iputils-ping \
-    libcurl4 \
-    libunwind8 \
-    netcat \
-    libssl1.0
+# ####
+# apt-get install -y --no-install-recommends \
+#     ca-certificates \
+#     curl \
+#     jq \
+#     git \
+#     iputils-ping \
+#     libcurl4 \
+#     libunwind8 \
+#     netcat \
+#     libssl1.0
 
-rm -rf /var/lib/apt/lists/* \
-curl -LO https://dl.k8s.io/release/v1.23.0/bin/linux/amd64/kubectl \
-chmod +x ./kubectl \
-mv ./kubectl /usr/local/bin/kubectl \
-curl -LO https://get.helm.sh/helm-v3.7.2-linux-amd64.tar.gz \
-tar xvf helm-v3.7.2-linux-amd64.tar.gz \
-mv linux-amd64/helm /usr/local/bin \
-rm helm* && rm -r linux-*
+# rm -rf /var/lib/apt/lists/* \
+# curl -LO https://dl.k8s.io/release/v1.23.0/bin/linux/amd64/kubectl \
+# chmod +x ./kubectl \
+# mv ./kubectl /usr/local/bin/kubectl \
+# curl -LO https://get.helm.sh/helm-v3.7.2-linux-amd64.tar.gz \
+# tar xvf helm-v3.7.2-linux-amd64.tar.gz \
+# mv linux-amd64/helm /usr/local/bin \
+# rm helm* && rm -r linux-*
 
 
 
